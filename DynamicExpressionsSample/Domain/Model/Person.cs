99a4a997a704e2a2f4ea342b416b432ab4ad9242
aartiomov <andrej.artiomov@gmail.com>
2015-10-01 18:31:10 +0300

    - ADD. Person model.

---
+++ DynamicExpressionsSample/Domain/Model/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DynamicExpressionsSample.Domain.Model
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string OrdersCount { get; set; }      
+        public string Gender { get; set; }  
+    }
+}
