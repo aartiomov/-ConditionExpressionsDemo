0464b8d20a52b62f3cc2325a982f26c6c930e0ef
aartiomov <andrej.artiomov@gmail.com>
2015-10-02 10:45:09 +0300

    - ADD. WebAPI controller. Sample data file (not completed).

---
+++ DynamicExpressionsSample/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using DynamicExpressionsSample.Domain.Model;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Http;
+
+namespace DynamicExpressionsSample.Controllers
+{
+    /// <summary>
+    /// Web API Controller with get method. It returns customers.
+    /// </summary>
+    public class CustomerController : ApiController
+    {
+        // GET api/customer
+        public IEnumerable<Customer> Get()
+        {
+            var path = System.Web.Hosting.HostingEnvironment.MapPath(@"~/customers.json");            
+            IEnumerable<Customer> customers = JsonConvert.DeserializeObject<IEnumerable<Customer>>(File.ReadAllText(path));  
+            
+            return customers;
+        }
+    }
+}
--- DynamicExpressionsSample/Domain/Model/Customer.cs
+++ DynamicExpressionsSample/Domain/Model/Customer.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 
 namespace DynamicExpressionsSample.Domain.Model
 {
-    public class Person
+    public class Customer
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime? BirthDate { get; set; }
-        public string OrdersCount { get; set; }      
+        public int OrdersCount { get; set; }      
         public string Gender { get; set; }  
     }
 }
